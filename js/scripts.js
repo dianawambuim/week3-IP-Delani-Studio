f704eabae9f89cea0316da238ece1b1ff52739f5
dianawambuim <dianawambuim@gmail.com>
2022-12-06 00:35:30 +0300

    updated services

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("#design").click(function() {
+      if($('#designp').hasClass('d-none')){
+      $('#designp').removeClass('d-none');
+      }else{
+      $('#designp').addClass('d-none');
+      }
+  });
+  
+});
+
+(function(){
+  
+});
+
+
